50ec90baf9475118d64a14ccaf27331f40e103f4
Asyraf Ahmad <asyraf@time.com.my>
2021-07-14 12:57:37 +0800

    Part 07: DB design, model and migration creation

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,18 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('fullName');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('userType')->default('user');
+            $table->boolean('isActivated')->default(0);
+            $table->string('passwordResetCode')->nullable();
+            $table->string('activationCode')->nullable();
+            $table->string('socialType')->nullable();
             $table->timestamps();
+
+            // $table->rememberToken();
+            // $table->timestamp('email_verified_at')->nullable();
         });
     }
 
